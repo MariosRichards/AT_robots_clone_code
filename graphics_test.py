fbffdeefc88c2e67e006470cfcd578152387cd16
Marios Richards <Marios.Richards@gmail.com>
2016-08-20 23:10:51 +0200

    Wall clipping, turning, tank facing implemented

--- graphics_test.py
+++ graphics_test.py
@@ -10,22 +10,44 @@ import numpy as np
 import time
 
 
+class CPU:
+    
+    # registers
+    
+    def __init__(self, tank):
+        self.physical_tank = tank
+    
+    def run_cpu(self):
+        self.physical_tank.change_facing(self.physical_tank.facing + (np.random.rand()-.5)*50 )
+        
+        
+    # 
+
+
 class Tank:
    #'Tank class'
-
-   tank_points = np.array( [ [-2,-3], [2,-3], [0,3] ] )/np.sqrt(2*2 + 3*3)
+    # standard points for tank facing up (x,y)
+    # (-2, -3), (2,-3), (0,3)
+   tank_points = np.array( [ [-3,2], [-3,-2], [3,0] ] )/np.sqrt(2*2 + 3*3)
    
-   def __init__(self, xpos, ypos, radius, facing, window, xvel, yvel):
+   #tank_points = np.array( [ [-2,-3], [2,-3], [0,3] ] )/np.sqrt(2*2 + 3*3)   
+   #tank_points = -tank_points
+   max_turn = 10
+   
+   
+   def __init__(self, xpos, ypos, radius, facing, window, speed):
       self.xpos = xpos
       self.ypos = ypos
-      self.xvel = xvel
-      self.yvel = yvel      
+      self.speed = 20
+     
       self.radius = radius
       self.facing = facing
       facing_radians = (facing/128) * np.pi
-      rot_facing = [ [np.cos(facing_radians), -np.sin(facing_radians)], [np.sin(facing_radians), np.cos(facing_radians)] ]
-      rot_tank = self.radius*np.dot(self.tank_points,rot_facing)
       
+      rot_facing = [ [np.cos(facing_radians), np.sin(facing_radians)], [-np.sin(facing_radians), np.cos(facing_radians)] ]
+      
+      rot_tank = self.radius*np.dot(self.tank_points,rot_facing)
+    
       self.tank_image = Polygon( Point(self.xpos + rot_tank[0,0],
                                             self.ypos + rot_tank[0,1]),
                     Point(self.xpos + rot_tank[1,0],
@@ -33,22 +55,71 @@ class Tank:
                     Point(self.xpos + rot_tank[2,0],
                         self.ypos + rot_tank[2,1]), )      
     
-      self.tank_image.setFill("red")                 # color it
-      self.tank_image.draw(window)                        # place ball on window    
+      self.tank_image.setFill("red")     # color it
+      self.tank_image.draw(window)       # place ball on window
+      
+      self.xvel = self.speed*np.cos(facing_radians)
+      self.yvel = self.speed*np.sin(facing_radians)         
+      
+      self.cpu = CPU(self)
     
-   def update(self, delta_t, width, height):
+   def change_facing(self, desired_facing):
+        desired_turn = ( ( desired_facing - self.facing +128) % 256 ) -128
+        if desired_turn > self.max_turn:
+            desired_turn = self.max_turn
+        elif desired_turn < -self.max_turn:
+            desired_turn = -self.max_turn
+        self.facing = (self.facing + desired_turn) % 256
+        
+        print(self.facing)
+    
+   def update(self, delta_t, width, height, window):
+   
+        self.cpu.run_cpu()
+   
+        # 
+        facing_radians = (self.facing/128) * np.pi
+        self.xvel = self.speed*np.cos(facing_radians)
+        self.yvel = self.speed*np.sin(facing_radians)          
+   
         xmov = self.xvel * delta_t           # identify new x
         ymov = self.yvel * delta_t           # and y positions
-        self.tank_image.move(xmov , ymov)     # move the ball the required amount
+
+        if (xmov + self.xpos + self.radius) > width:
+            xmov = width - self.radius - self.xpos
+
+        elif (xmov + self.xpos - self.radius) < 0 :
+            xmov = self.radius - self.xpos
+        
+        if (ymov + self.ypos + self.radius) > height:
+            ymov = height - self.radius - self.ypos
+
+        elif (ymov + self.ypos - self.radius) < 0 :
+            ymov = self.radius - self.ypos
+
+        #self.tank_image.move(xmov, ymov)    
+
         self.xpos += xmov     # reset x_i and y_i values so that
         self.ypos += ymov     # we can use loop to move it agai
+        
+        self.tank_image.undraw()
+        
 
-        if (self.xpos + self.radius) > width  or (self.xpos - self.radius) < 0 :
-            self.xvel = -self.xvel  # reverse direction of velocity        
-        
-        if (self.ypos + self.radius) > height or (self.ypos - self.radius) < 0 :
-            self.yvel = -self.yvel  # reverse direction of velocity           
-        
+        rot_facing = [ [np.cos(facing_radians), np.sin(facing_radians)], [-np.sin(facing_radians), np.cos(facing_radians)] ]
+
+        rot_tank = self.radius*np.dot(self.tank_points,rot_facing)
+
+        self.tank_image = Polygon( Point(self.xpos + rot_tank[0,0],
+                                            self.ypos + rot_tank[0,1]),
+                    Point(self.xpos + rot_tank[1,0],
+                        self.ypos + rot_tank[1,1]),
+                    Point(self.xpos + rot_tank[2,0],
+                        self.ypos + rot_tank[2,1]), )      
+
+        self.tank_image.setFill("red")     # color it
+        self.tank_image.draw(window)       # place ball on window
+
+            
 def main():
     width = 500                         # set up window
     height = 500
@@ -61,49 +132,35 @@ def main():
     radius = 20            # establish initial values
     x_i = width/2 + radius
     y_i = height/2 + radius
-    facing = 128 # 
-    v_x = 10    # set up x and y velocities
-    v_y = 0    
+    facing = 0 # 
+    speed = 1
+ 
+    object_list = []
     
-    initial_tank = Tank(x_i, y_i, radius, facing, w, v_x, v_y)
-    # standard points for tank facing up (x,y)
-    # (-2, -3), (2,-3), (0,3)
+    initial_tank = Tank(x_i, y_i, radius, facing, w, speed)
+    
+    second_tank = Tank(x_i+100, y_i+100, radius, facing, w, speed)
+    object_list.append(initial_tank)
+    object_list.append(second_tank)
+
     
 
-    # facing_radians = (facing/128) * np.pi
-    # tank_points = np.array( [ [-2,-3], [2,-3], [0,3] ] )/np.sqrt(2*2 + 3*3)
-    # rot_facing = [ [np.cos(facing_radians), -np.sin(facing_radians)], [np.sin(facing_radians), np.cos(facing_radians)] ]
-    
-    # rot_tank = size*np.dot(tank_points,rot_facing)
-    
-    # tank = Polygon( Point(x_i + rot_tank[0,0],
-                        # y_i + rot_tank[0,1]),
-                    # Point(x_i + rot_tank[1,0],
-                        # y_i + rot_tank[1,1]),
-                    # Point(x_i + rot_tank[2,0],
-                        # y_i + rot_tank[2,1]), )
-   # ball = Circle(Point(x_i,y_i), radius)   # establish ball as a circle
 
     
     
     delta_t = 1                        # establish time interval
     # start timer loop   
-
+    game_cycle = 0
     while (True):
+        game_cycle += 1
 
-        # x_f = x_i + v_x * delta_t           # identify new x
-        # y_f = y_i + v_y * delta_t           # and y positions
+        print("STATUS: ",game_cycle)    # report values in Terminal window
+        for obj in object_list:
+            obj.update(delta_t, width, height, w)
     
-        print("STATUS:XXXX")    # report values in Terminal window
-        initial_tank.update(delta_t, width, height)
-    
-        # tank.move(x_f - x_i, y_f - y_i)     # move the ball the required amount
-        
-        # x_i = x_f                           # reset x_i and y_i values so that
-        # y_i = y_f                           # we can use loop to move it agai
+
         time.sleep(1/30)
-        # if (x_f + radius) > width or (x_f - radius) < 0 :
-            # v_x = v_x * -1  # reverse direction of velocity        
+
 
 if __name__ == "__main__":
     main()
